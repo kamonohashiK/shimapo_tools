9ae3bde9a377f214323c2e7b6d9b67b795ac940c
kamonohashiK <kamonohashiy@gmail.com>
2023-09-08 21:09:35 +0900

    #1 変換スクリプトと変換後のファイルを追加

--- main.go
+++ main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type IslandSummary struct {
+	Name   string  `json:"name"`
+	Kana   string  `json:"kana"`
+	EnName string  `json:"en_name"`
+	Lat    float64 `json:"lat"`
+	Lng    float64 `json:"lng"`
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var combinedData []IslandSummary
+
+	// CSVファイルを開く
+	file, err := os.Open("islands.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	for {
+		island, err := reader.Read()
+		if err != nil {
+			break
+		}
+
+		lat, _ := strconv.ParseFloat(island[4], 64)
+		lng, _ := strconv.ParseFloat(island[5], 64)
+		r := IslandSummary{
+			Name:   island[0],
+			Kana:   island[1],
+			EnName: island[2],
+			Lat:    lat,
+			Lng:    lng,
+		}
+
+		combinedData = append(combinedData, r)
+	}
+
+	// 結合されたJSONデータをJSONに変換
+	combinedJSON, err := json.Marshal(combinedData)
+	if err != nil {
+		panic(err)
+	}
+
+	// 結合されたJSONデータをファイルに出力
+	outputFile, err := os.Create("islands.json")
+	if err != nil {
+		panic(err)
+	}
+	defer outputFile.Close()
+
+	_, err = outputFile.Write(combinedJSON)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("jsonファイルへの出力完了")
 }
